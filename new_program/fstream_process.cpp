60dbf15dd3cdf4d5b48cd6144f414788a7b9c65a
VikingOfValhalla <tom.wilson@nordtechsystems.com>
2022-08-22 22:04:15 -0400

    Identified getline in fstream was causing issues because of spaces assuming new line characters.

--- new_program/fstream_process.cpp
+++ new_program/fstream_process.cpp
@@ -8,10 +8,10 @@ std::string file_to_string(std::string file_location) {
 
     file_handle.open(file_location);
     if (file_handle.is_open()) {
-        while(getline(file_handle, lines)) {
-            std::string delimiter(",");
+        // no delimiter causes getline() to not work correctly because it assumes spaces are new lines
+        while(getline(file_handle, lines, ';')) {
             file_handle >> lines;
-            std::cout << lines << delimiter << std::endl;;
+            std::cout << lines;
         }
         file_handle.close();
     }
@@ -19,7 +19,10 @@ std::string file_to_string(std::string file_location) {
 }
 
 void string_to_file(std::string file_location, 
-                           std::string input_value) {
-    // do something
+                    std::string input_value) {
+    std::ofstream file_name;
+    file_name.open(file_location, std::ios_base::app);
+    file_name << std::endl << input_value;
+    file_name.close();
 }
 
--- new_program/main.cpp
+++ new_program/main.cpp
@@ -6,17 +6,53 @@
 #include "syntax_validation.cpp"
 #include "fstream_process.cpp"
 
+void print_start_menu() {
+    std::cout << "1) Play Game\n"; 
+    std::cout << "2) Edit/Create Flash Cards\n"; 
+    std::cout << "3) Exit\n"; 
+}
 
 int main () {
-    /*    
+    
+    // start program -- start menu
+    // [ ] - play game
+    // [ ] - edit/create flash cards
+    // [ ] - exit
+
+    // ~~ play game ~~
+    // [ ] - select group
+    //    [ ] - while loop for quanity in group list is > 0
+
+    // ~~ edit/create flash cards
+    // [ ] - edit flash cards
+    //    [ ] - list of current flash cards
+    //    [ ] - input # to edit flash card
+    //        [ ] - selection: edit / delete
+    //            [ ] - edit: highlights # of flash card
+    //                [ ] - re-enter entire flash card using syntax
+    //            [ ] - delete: removes flash card # line from text file
+
+    // [ ] - create flash cards
+    //    [ ] - ask user for input {"Question"},{"Answer"},{"Group"}
+    //    [ ] - validates user input based on syntax
+    //        [ ] - if correct save to new line of text file
+    //        [ ] - if incorrect, throw error, retry
+
+    // ~~ exit ~~ 
+
+
+    std::string file_location = "../data/data_structures.txt";
+    string_to_file(file_location, "Here is some new text");
+    
+    /*
     std::string test = return_alphanumeric("(this is a test)");
     bool test_2 = syntax_validator(test);
     std::cout << std::boolalpha  <<  test_2 << '\n';
     */
 
-    
     file_to_string("../data/data_structures.txt");
     
+    /*
     Flash_Card fc;
     std::string q = "this is a question\n";
     fc.set_question(q);
@@ -25,6 +61,7 @@ int main () {
     std::string a = "This is an answer\n";
     fc.set_correct_answer(a);
     fc.print_answer();
-    
+    */
+
     return 0;
 }
